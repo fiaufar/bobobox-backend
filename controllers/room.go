7c13b10f1b4e5c0de01f03dc7dd34bdc0371ed10
fiaufar <fiaufar@gmail.com>
2022-02-08 08:51:04 +0700

    Adding total price on response room

--- controllers/room.go
+++ controllers/room.go
@@ -2,45 +2,44 @@ package controllers
 
 import (
 	"assignment/models"
-	"fmt"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
 
 func (db *InDB) GetRoomAvailable(c *gin.Context) {
 	var (
-		person []models.Room
+		data   []models.Room
 		result gin.H
 	)
-	//maps := c.Request.URL.Query()
-	//fmt.Println(maps)
-	//fmt.Println(maps["id"][0])
 
-	//str := ""
-	//for _, value := range maps {
-	//	str = str + value
-	//}
 	checkinDate := c.Query("checkin_date")
 	checkoutDate := c.Query("checkout_date")
 	roomQty := c.Query("room_qty")
 	roomTypeId := c.Query("room_type_id")
-	fmt.Println(checkinDate)
-	fmt.Println(checkoutDate)
-	fmt.Println(roomQty)
-	fmt.Println(roomTypeId)
-	//err := db.DB.Where("id = ?", id).First(&person).Error
-	//err := db.DB.Raw("SELECT * FROM rooms").Scan(&person).Error
-	err := db.DB.Preload("Prices", "date IN (?)", []string{checkinDate, checkoutDate}).Where("room_status = 'available'").Where("room_type_id = ?", roomTypeId).Find(&person).Error
-	//fmt.Println(person)
+
+	err := db.DB.Preload("Prices", "date IN (?)", []string{checkinDate, checkoutDate}).Where("room_status = 'available'").Where("room_type_id = ?", roomTypeId).Find(&data).Error
+	totalPrice := 0
+	if len(data) > 0 && len(data[0].Prices) > 0 {
+		totalPrice = data[0].Prices[0].Price
+	}
+
 	if err != nil {
 		result = gin.H{
-			"result": err.Error(),
-			"count":  0,
+			"available_room": err.Error(),
+			"total_price":    0,
+			"checkout_date":  checkoutDate,
+			"checkin_date":   checkinDate,
+			"room_type_id":   roomTypeId,
+			"room_qty":       roomQty,
 		}
 	} else {
 		result = gin.H{
-			"result": person,
-			"count":  len(person),
+			"available_room": data,
+			"total_price":    totalPrice,
+			"checkout_date":  checkoutDate,
+			"checkin_date":   checkinDate,
+			"room_type_id":   roomTypeId,
+			"room_qty":       roomQty,
 		}
 	}
 
--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module assignment
 
 go 1.15
+
+require (
+	github.com/gin-gonic/gin v1.7.7
+	github.com/go-sql-driver/mysql v1.6.0
+	github.com/gorilla/mux v1.8.0
+	github.com/jinzhu/gorm v1.9.16
+)
